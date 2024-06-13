b8e20914b9830f7a8d8f7766df2d7a08d9cd4bac
BONNIN Simon <Simon.Bonnin@lisi-aerospace.com>
2024-06-13 10:41:46 +0200

    Commit

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
-// Exemple d'interaction simple
-document.addEventListener("DOMContentLoaded", () => {
-    const navLinks = document.querySelectorAll("nav ul li a");
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-links a');
+
     navLinks.forEach(link => {
-        link.addEventListener("click", (event) => {
+        link.addEventListener('click', event => {
             event.preventDefault();
-            const targetId = link.getAttribute("href").substring(1);
+            const targetId = event.target.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
-            
-            targetSection.scrollIntoView({ behavior: "smooth" });
+
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
         });
     });
 });
